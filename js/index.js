af035cb92f9cd960279c405f9118eb3ee86e4376
raffifnibras <raffifgickypraloyo@gmail.com>
2024-10-20 22:03:12 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'aku punya sesuatu buat elaa'.split('')
+const text = 'aku punya sesuatu buat salsa'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
